a6ffdb84bdf606b7432033a9ef037cf76ed6b342
Lama Almqayyad <lamaalmqayyad@Lamas-MacBook-Pro.local>
2023-02-01 18:36:55 +0100

    spraak

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import Dictionary from "./Dictionary";
 
 const dictionary = new dictionary("en.json", "no.json");
 
-await dictionary.setLanguage("en");
+await dictionary.setLanguage("no");
 
 const value = dictionary.get("welcome_massage");
 console.log(value);
