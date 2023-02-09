37dbba3b58ed9018fd651f8bb8d4484414b2cdab
Lama Almqayyad <lamaalmqayyad@Lamas-MacBook-Pro.local>
2023-02-09 16:19:27 +0100

    lama

--- test.mjs
+++ test.mjs
@@ -1,11 +1,11 @@
 import Dictionary from "./Dictionary.mjs";
 
-console.log("Test for å skrive ut");
+  console.log("Test for å skrive ut");
 
 const languages = ["./no.json", "./en.json"];
 const dictionary = new Dictionary(...languages);
 
-await dictionary.setLanguage("no");
+await dictionary.setLanguage("no"); 
 
 const value = dictionary.translate("welcome_massage");
 console.log('test', value);
